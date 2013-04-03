9823ee875721b27abaa3ac1a6fc2c8f6f08a7212
David Long <d.long@theitjobboard.co.uk>
2013-04-03 16:51:54 +0100

    Added sorter and applied to template

--- js/main.js
+++ js/main.js
@@ -1,37 +1,40 @@
 /**
- * 
+ *
+ * Author: David Long (dlong06)
+ **/
+
+
+/** namespace to product against third-party libraries for the Yum product
+ * @namespace Yum
  */
+var Yum = window.Yum || {};
+
+Yum.products = new Yum.ProductList();
+Yum.basket   = new Yum.Basket();
 
 function renderPage() {
 
-    var products = new Yum.ProductList();
-    var basket = new Yum.Basket();
-
     showPage( 'home_page' );
 
+    /** Setup our product database **/
     /** juices **/
-    products.addProduct( new Yum.Product( 0, 'Lime Juice', 'lime', 5.99, 50 ) );
-    products.addProduct( new Yum.Product( 1, 'Apple Juice', 'apple', 5.99, 15 ) );
-    products.addProduct( new Yum.Product( 2, 'Orange Juice', 'orange', 5.99, 30 ) );
-    products.addProduct( new Yum.Product( 3, 'Lemon Juice', 'lemon', 5.99, 20 ) );
-    products.addProduct( new Yum.Product( 4, 'Grapefruit Juice', 'grapefruit', 5.99, 10 ) );
+    Yum.products.addProduct( new Yum.Product( 0, 'Lime Juice', 'lime', 6.99, 50 ) );
+    Yum.products.addProduct( new Yum.Product( 1, 'Apple Juice', 'apple', 4.99, 15 ) );
+    Yum.products.addProduct( new Yum.Product( 2, 'Orange Juice', 'orange', 4.99, 30 ) );
+    Yum.products.addProduct( new Yum.Product( 3, 'Lemon Juice', 'lemon', 5.49, 20 ) );
+    Yum.products.addProduct( new Yum.Product( 4, 'Grapefruit Juice', 'grapefruit', 6.99, 10 ) );
     /** milk smoothies **/
-    products.addProduct( newMilkSmoothie( 5, 'Strawberry Milk', 'stawberrymilk', 11.99, 90 ) );
-    products.addProduct( newMilkSmoothie( 6, 'Blueberry Milk', 'blueberrymilk', 11.99, 100 ) );
-    products.addProduct( newMilkSmoothie( 7, 'Banana Milk', 'bananamilk', 11.99, 6 ) );
-    products.addProduct( newMilkSmoothie( 8, 'Summer Fruits Milk', 'summerfruitsmilk', 11.99, 2 ) );
+    Yum.products.addProduct( newMilkSmoothie( 5, 'Strawberry Milk', 'stawberrymilk', 9.99, 90 ) );
+    Yum.products.addProduct( newMilkSmoothie( 6, 'Blueberry Milk', 'blueberrymilk', 11.99, 100 ) );
+    Yum.products.addProduct( newMilkSmoothie( 7, 'Banana Milk', 'bananamilk', 9.99, 6 ) );
+    Yum.products.addProduct( newMilkSmoothie( 8, 'Summer Fruits Milk', 'summerfruitsmilk', 11.99, 2 ) );
     /** non-milk smoothies **/
-    products.addProduct( newSmoothie( 9, 'Strawberry &amp; Banana', 'stawberrybanana', 11.99, 4, true ) );
-    products.addProduct( newSmoothie( 10, 'Raspberry Pie', 'raspberrypie', 11.99, 4, true ) );
-    products.addProduct( newSmoothie( 11, 'Blueberry, Rasberry &amp; Blackberry', 'blueberryraspberryblackberry', 11.99, 6, false ) );
+    Yum.products.addProduct( newSmoothie( 9, 'Strawberry &amp; Banana', 'stawberrybanana', 9.99, 4, true ) );
+    Yum.products.addProduct( newSmoothie( 10, 'Raspberry Pie', 'raspberrypie', 10.99, 4, true ) );
+    Yum.products.addProduct( newSmoothie( 11, 'Blueberry, Rasberry &amp; Blackberry', 'blueberryraspberryblackberry', 12.99, 6, false ) );
+    Yum.products.addProduct( newSmoothie( 12, 'Forest Fruits', 'forestfruits', 15.99, 10, false ) );
 
-    products.addProduct( newSmoothie( 12, 'Forest Fruits', 'forestfruits', 11.99, 10, false ) );
-
-    document.getElementById( 'basket' ).innerHTML = basket.render();
-    document.getElementById( 'juices' ).innerHTML = products.renderCategoryStock( 'juice' );
-    document.getElementById( 'smoothies' ).innerHTML = products.renderCategoryStock( 'smoothie' );
-    document.getElementById( 'glutenfree' ).innerHTML = products.renderGlutenFreeStock();
-    document.getElementById( 'dairyfree' ).innerHTML = products.renderDairyFreeStock();
+    pageSorter( 'name' );
 
 }
 
@@ -52,4 +55,35 @@ function showPage( pageName ) {
     document.getElementById( pageName ).className = "page visible";
 
     return false; //stop the hyperlink from redirecting to another page
+}
+
+function pageSorter( sortBy ) {
+    var sorter = new Yum.ProductListSorter( Yum.products.products );
+    switch( sortBy ) {
+        case 'lowprice' :
+            Yum.products.products = sorter.priceAscending();
+            break;
+        case 'highprice' :
+            Yum.products.products = sorter.priceDescending();
+            break;
+        case 'popularity' :
+            Yum.products.products = sorter.popularity();
+            break;
+        case 'name' :
+        default :
+            Yum.products.products = sorter.name();
+            break;
+    }
+    //render pages
+    renderPageSections();
+
+    return false; //stop the hyperlink from redirecting to another page
+}
+
+function renderPageSections() {
+    document.getElementById( 'basket' ).innerHTML = Yum.basket.render();
+    document.getElementById( 'juices' ).innerHTML = Yum.products.renderCategoryStock( 'juice' );
+    document.getElementById( 'smoothies' ).innerHTML = Yum.products.renderCategoryStock( 'smoothie' );
+    document.getElementById( 'glutenfree' ).innerHTML = Yum.products.renderGlutenFreeStock();
+    document.getElementById( 'dairyfree' ).innerHTML = Yum.products.renderDairyFreeStock();
 }
---
+++ js/product_list_sorter.js
@@ -0,0 +1,58 @@
+/**
+ * Product List Sorter
+ *
+ * Class for handling product ordering
+ *
+ * Author: David Long (dlong06)
+ **/
+
+/** namespace to product against third-party libraries for the Yum product
+ * @namespace Yum
+ */
+var Yum = window.Yum || {};
+
+Yum.lastSort = 'name';
+
+/**
+ * ProductListSorter
+ * Changes the order of products
+ * @class ProductListSorter
+ */
+Yum.ProductListSorter = (function( productList ) {
+    this.products = productList;
+
+    /**
+     * name
+     * Sort by name, a custom function to sort products by name.  Due to the structure of the content within the array
+     * the default sort is not valid for this filter.
+     * @returns {Array}
+     */
+    this.name = function() {
+        this.products.sort(
+            function(a,b) {
+                if (a.name < b.name) return -1;
+                if (a.name > b.name) return 1;
+                return 0;
+            });
+        Yum.lastSort = 'name';
+        return this.products;
+    };
+
+    this.priceAscending = function() {
+        this.products.sort(function(a,b){ return a.price - b.price });
+        Yum.lastSort = 'lowprice';
+        return this.products;
+    };
+
+    this.priceDescending = function() {
+        this.products.sort(function(a,b){ return b.price - a.price });
+        Yum.lastSort = 'highprice';
+        return this.products;
+    };
+
+    this.popularity = function() {
+        this.products.sort(function(a,b){ return a.quantity - b.quantity });
+        Yum.lastSort = 'popularity';
+        return this.products;
+    };
+});
